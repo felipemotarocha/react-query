f25ab42569b2b5ddbc9b657ca16fadc4a480fd98
Felipe Rocha <felipemota.rocha@gmail.com>
2022-11-22 17:51:28 -0300

    feat: add useQuery and useMutation hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const queryClient = useQueryClient();
 
-  useEffect(() => {
-    axios.get("http://localhost:8080/todos").then((res) => setTodos(res.data));
-  }, []);
+  const { data, isLoading, error } = useQuery("todos", () =>
+    axios.get("http://localhost:8080/todos").then((response) => response.data)
+  );
+
+  const mutation = useMutation({
+    mutationFn: ({ todoId, completed }) => {
+      return axios
+        .patch(`http://localhost:8080/todos/${todoId}`, {
+          completed,
+        })
+        .then((response) => response.data);
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData("todos", (currentData) =>
+        currentData.map((todo) => (todo.id === data.id ? data : todo))
+      );
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Carregando...</div>;
+  }
+
+  if (error) {
+    return <div className="loading">Algo deu errado!</div>;
+  }
 
   return (
     <div className="app-container">
       <div className="todos">
         <h2>Todos & React Query</h2>
-        {todos.map((todo) => (
+        {data.map((todo) => (
           <div
+            onClick={() =>
+              mutation.mutate({ todoId: todo.id, completed: !todo.completed })
+            }
             className={`todo ${todo.completed && "todo-completed"}`}
             key={todo.id}
           >
